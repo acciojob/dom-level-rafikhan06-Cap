f69cbfd7cd9e04f31431831eb63ab3662edfb87c
rafikhan06-Cap <rafikhan3488@gmail.com>
2025-10-17 00:11:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const document.querySelector("#level");
+let count = 0;
+let previous = level.previousElementSibling;
+while (previous) {
+	count++;
+	previous = previous.previousElementSibling;
+}
+alert("The level of the element is : "+count)
